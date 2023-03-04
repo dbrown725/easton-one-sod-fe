cd609f1dd3eda75099684cfbc93736a9e0ed45eb
dbrown725 <davidbrown772255@gmail.com>
2023-03-04 17:02:20 -0500

    Song Form formatting changes

--- src/components/SongForm.tsx
+++ src/components/SongForm.tsx
@@ -1,8 +1,9 @@
-import { IonButton, IonCol, IonGrid, IonInput, IonItem, IonLabel, IonList, IonRadio, IonRadioGroup, IonRow, IonTextarea } from '@ionic/react';
+import { IonButton, IonInput, IonItem, IonLabel, IonList, IonTextarea } from '@ionic/react';
 import { useEffect, useRef, useState } from 'react';
-import {Song, SongFormProps} from '../common/types';
+import { Song, SongFormProps } from '../common/types';
 import DOMPurify from 'dompurify';
 import { useHistory, useLocation } from 'react-router';
+import './SongForm.css';
 
 const SongForm: React.FC<SongFormProps> = (props) => {
 
@@ -10,10 +11,10 @@ const SongForm: React.FC<SongFormProps> = (props) => {
   const [id, setId] = useState<number | null | undefined>();
   const [message, setMessage] = useState<string | null | undefined>('');
   const [title, setTitle] = useState<string | number | null | undefined>('');
-  const [bandName, setBandName] = useState<string |number | null | undefined>('');
+  const [bandName, setBandName] = useState<string | number | null | undefined>('');
   const [songName, setSongName] = useState<string | number | null | undefined>('');
-  const [link, setLink] = useState<string | number |null | undefined>('');
-  const [playlist, setPlaylist] = useState<string | number |null | undefined>(playlistName);
+  const [link, setLink] = useState<string | number | null | undefined>('');
+  const [playlist, setPlaylist] = useState<string | number | null | undefined>(playlistName);
   const location = useLocation<{ song: Song }>();
   const history = useHistory();
 
@@ -31,7 +32,7 @@ const SongForm: React.FC<SongFormProps> = (props) => {
     const unlisten = history.listen((location) => {
       //myStateRef.current is the previous page
       //clear everything if you just left /page/Submit
-      if(myStateRef.current === '/page/Submit') {
+      if (myStateRef.current === '/page/Submit') {
         clearForm();
         clearState();
       }
@@ -44,17 +45,17 @@ const SongForm: React.FC<SongFormProps> = (props) => {
   // End logic related to clearing form when user navigates away without submitting.
 
   useEffect(() => {
-    if(location.state){
-      if(location.state.song) {
+    if (location.state) {
+      if (location.state.song) {
         setId(location.state.song.id);
-        if(location.state.song.message) {
+        if (location.state.song.message) {
           setMessage(location.state.song.message);
         }
         setTitle(location.state.song.title);
         setBandName(location.state.song.bandName);
         setSongName(location.state.song.songName);
         setLink(location.state.song.link);
-        if(location.state.song.playlist) {
+        if (location.state.song.playlist) {
           setPlaylist(location.state.song.playlist);
         } else {
           setPlaylist(playlistName);
@@ -71,7 +72,7 @@ const SongForm: React.FC<SongFormProps> = (props) => {
   }
 
   const submitData = async (buttonIdentifier: String) => {
-    if((!title || !bandName || !songName || !link) &&
+    if ((!title || !bandName || !songName || !link) &&
       buttonIdentifier !== 'cancelUpdateSodSong' && buttonIdentifier !== 'cancelNewOrBPSong') {
       return;
     }
@@ -82,15 +83,15 @@ const SongForm: React.FC<SongFormProps> = (props) => {
     props.song.link = sanitizeData(String(link));
     props.song.playlist = sanitizeData(String(playlist));
 
-    if(buttonIdentifier === 'newSodSong') {
+    if (buttonIdentifier === 'newSodSong') {
       await props.sodInsertCallback();
-    } else if(buttonIdentifier === 'updateSodSong') {
+    } else if (buttonIdentifier === 'updateSodSong') {
       await props.sodUpdateCallback();
-    } else if(buttonIdentifier === 'cancelUpdateSodSong') {
+    } else if (buttonIdentifier === 'cancelUpdateSodSong') {
       await props.sodCancelUpdateCallback();
-    } else if(buttonIdentifier === 'bullpenSong') {
+    } else if (buttonIdentifier === 'bullpenSong') {
       await props.bpCallback();
-    } else if(buttonIdentifier === 'cancelNewOrBPSong') {
+    } else if (buttonIdentifier === 'cancelNewOrBPSong') {
       history.goBack();
     } else {
       console.log('fell through');
@@ -119,103 +120,71 @@ const SongForm: React.FC<SongFormProps> = (props) => {
 
   return (
     <IonList>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <IonGrid>
-            <IonRow>
-              <IonCol>
-                <IonItem>
-                  <IonLabel position="stacked">Message (for email only)</IonLabel>
-                  <IonTextarea value={message} onIonChange={(e) => setMessage((e.target as HTMLIonTextareaElement).value)} placeholder="Enter a message for your email"></IonTextarea>
-                </IonItem>
-              </IonCol>
-            </IonRow>
+      <form onSubmit={(e) => e.preventDefault()} className='ion-padding'>
+        <IonItem>
+          <IonLabel position="floating">Message (for email only)</IonLabel>
+          <IonTextarea value={message} onIonChange={(e) => setMessage((e.target as HTMLIonTextareaElement).value)} placeholder="Enter a message for your email"></IonTextarea>
+        </IonItem>
 
-            <IonRow>
-              <IonCol>
-                <IonItem>
-                  <IonLabel position="stacked">Title</IonLabel>
-                  <IonInput autofocus value={title} onIonChange={(e) => setTitle((e.target as HTMLIonInputElement).value)} required clear-input placeholder="Enter title"></IonInput>
-                </IonItem>
-              </IonCol>
-            </IonRow>
+        <IonItem>
+          <IonLabel position="floating">Title</IonLabel>
+          <IonInput autofocus value={title} onIonChange={(e) => setTitle((e.target as HTMLIonInputElement).value)} required clear-input placeholder="Enter title"></IonInput>
+        </IonItem>
 
-            <IonRow>
-              <IonCol>
-                <IonItem>
-                  <IonLabel position="stacked">Band/Artist</IonLabel>
-                  <IonInput value={bandName} onIonChange={(e) => setBandName((e.target as HTMLIonInputElement).value)} required clear-input placeholder="Enter band or artist's name"></IonInput>
-                </IonItem>
-              </IonCol>
-            </IonRow>
+        <IonItem>
+          <IonLabel position="floating">Band/Artist</IonLabel>
+          <IonInput value={bandName} onIonChange={(e) => setBandName((e.target as HTMLIonInputElement).value)} required clear-input placeholder="Enter band or artist's name"></IonInput>
+        </IonItem>
 
-            <IonRow>
-              <IonCol>
-                <IonItem>
-                  <IonLabel position="stacked">Song</IonLabel>
-                  <IonInput value={songName} onIonChange={(e) => setSongName((e.target as HTMLIonInputElement).value)} required clear-input placeholder="Enter song name"></IonInput>
-                </IonItem>
-              </IonCol>
-            </IonRow>
+        <IonItem>
+          <IonLabel position="floating">Song</IonLabel>
+          <IonInput value={songName} onIonChange={(e) => setSongName((e.target as HTMLIonInputElement).value)} required clear-input placeholder="Enter song name"></IonInput>
+        </IonItem>
 
-            <IonRow>
-              <IonCol>
-                <IonItem>
-                  <IonLabel position="stacked">YouTube url</IonLabel>
-                  <IonInput value={link} onIonChange={(e) => setLink((e.target as HTMLIonInputElement).value)} required clear-input pattern="https://.*" type="url" placeholder="Enter the YouTube url: https://...."></IonInput>
-                </IonItem>
-              </IonCol>
-            </IonRow>
+        <IonItem>
+          <IonLabel position="floating">YouTube url</IonLabel>
+          <IonInput value={link} onIonChange={(e) => setLink((e.target as HTMLIonInputElement).value)} required clear-input pattern="https://.*" type="url" placeholder="Enter the YouTube url: https://...."></IonInput>
+        </IonItem>
 
-            <IonRow>
-              <IonCol>
-                <IonItem>
-                  <IonLabel position="stacked">YouTube Playlist</IonLabel>
-                  <IonInput value={playlist} readonly={true}></IonInput>
-                </IonItem>
-              </IonCol>
-            </IonRow>
+        <IonItem>
+          <IonLabel position="floating">YouTube Playlist</IonLabel>
+          <IonInput value={playlist} readonly={true}></IonInput>
+        </IonItem>
 
-            <IonRow>
-            {props.updateSODRequest &&
-              <>
-                <IonCol>
-                  <IonButton id="cancelUpdateSodSong" expand="block" type="submit" className="ion-margin-top" onClick={(e) => submitData('cancelUpdateSodSong')}>
-                    Cancel
-                  </IonButton>
-                </IonCol>
-                <IonCol>
-                  <IonButton id="updateSodSong" expand="block" type="submit" className="ion-margin-top" onClick={(e) => submitData('updateSodSong')}>
-                    Update Song of the Day
-                  </IonButton>
-                </IonCol>
-              </>
-            }
+        {props.updateSODRequest &&
+          <>
 
-            {!props.updateSODRequest &&
-              <>
-                <IonCol>
-                  <IonButton id="cancelNewOrBPSong" expand="block" type="submit" className="ion-margin-top" onClick={(e) => submitData('cancelNewOrBPSong')}>
-                    Cancel
-                  </IonButton>
-                </IonCol>
-                <IonCol>
-                  <IonButton id="bullpenSong" expand="block" type="submit" className="ion-margin-top" onClick={(e) => {submitData('bullpenSong')}}>
-                    Save to my Bullpen
-                  </IonButton>
-                </IonCol>
-                <IonCol>
-                  <IonButton id="newSodSong" expand="block" type="submit" className="ion-margin-top" onClick={(e) => submitData('newSodSong')}>
-                    Submit your Song of the Day
-                  </IonButton>
-                </IonCol>
-              </>
-            }
-            </IonRow>
-        </IonGrid>      
+            <IonButton id="cancelUpdateSodSong" expand="block" type="submit" className="ion-margin-top" onClick={(e) => submitData('cancelUpdateSodSong')}>
+              Cancel
+            </IonButton>
+            <IonButton id="updateSodSong" expand="block" type="submit" className="ion-margin-top" onClick={(e) => submitData('updateSodSong')}>
+              Update Song of the Day
+            </IonButton>
+
+          </>
+        }
+
+        {!props.updateSODRequest &&
+          <>
+
+            <IonButton id="cancelNewOrBPSong" type="submit" className="ion-margin-top" onClick={(e) => submitData('cancelNewOrBPSong')}>
+              Cancel
+            </IonButton>
+
+            <IonButton id="bullpenSong" type="submit" className="ion-margin-top" onClick={(e) => { submitData('bullpenSong') }}>
+              Save to my Bullpen
+            </IonButton>
+
+            <IonButton id="newSodSong" type="submit" className="ion-margin-top" onClick={(e) => submitData('newSodSong')}>
+              Submit your Song of the Day
+            </IonButton>
+
+          </>
+        }
 
       </form>
     </IonList>
   );
 }
- 
+
 export default SongForm;
