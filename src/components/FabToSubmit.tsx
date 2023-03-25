85fb3b14bd83b4fd31b8db29580b4e83abc01ca6
dbrown725 <davidbrown772255@gmail.com>
2023-03-25 16:47:09 -0400

    Hid New Song FAB when role = "GUEST"

--- src/components/FabToSubmit.tsx
+++ src/components/FabToSubmit.tsx
@@ -1,10 +1,15 @@
 import { IonFab, IonFabButton, IonIcon } from '@ionic/react';
 import { add } from 'ionicons/icons';
+import { useEffect } from 'react';
 import { useHistory } from 'react-router';
+import { role, refreshRole } from '../firebase';
 
 const FabToSubmit: React.FC = () => {
 
   const history = useHistory();
+  useEffect(() => {
+    refreshRole();
+  }, []);
 
   const handleFabClick = () => {
     history.push({
@@ -14,11 +19,16 @@ const FabToSubmit: React.FC = () => {
   }
 
   return (
-    <IonFab vertical="bottom" horizontal="end" slot="fixed">
-      <IonFabButton onClick={ () => handleFabClick()}>
-        <IonIcon icon={add} />
-      </IonFabButton>
-    </IonFab>
+    <>
+      {
+        (role === 'ADMIN' || role === 'SUBMITTER') &&
+        <IonFab vertical="bottom" horizontal="end" slot="fixed">
+          <IonFabButton onClick={ () => handleFabClick()}>
+            <IonIcon icon={add} />
+          </IonFabButton>
+        </IonFab>
+      }
+    </>
   );
 }
  
