901ca22a01a996c4699720507b4d6642f98f7871
dbrown725 <davidbrown772255@gmail.com>
2023-04-03 19:43:43 -0400

    SearchForSongs cleanup and performance improvements

--- src/components/SearchForSongs.tsx
+++ src/components/SearchForSongs.tsx
@@ -1,6 +1,6 @@
 import { useLazyQuery } from '@apollo/client';
 import { SearchbarChangeEventDetail, IonBadge, IonCol, IonGrid, IonItem, IonLabel, IonRow, IonSearchbar } from '@ionic/react';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import FabToSubmit from './FabToSubmit';
 import SongList from './SongList';
 import { GET_BAND_STATS, GET_SEARCH_RESULTS } from '../graphql/graphql';
@@ -20,16 +20,17 @@ const SearchForSongs: React.FC<SearchingForSongsProps> = (props) => {
 
   const [inputValue, setInputValue] = useState<string | undefined | null>();
 
+  const refTimer = useRef<number | undefined>(undefined);
+
+  const refTimerTrigger = useRef<number | undefined>(undefined);
+
   const [bandStats, setBandStats] = useState<BandStats[]>([]);
 
   const [bubbleDivId, setBubbleDivId] = useState<string | undefined>();
 
-  const WAIT_INTERVAL = 1000;
-
   const history = useHistory();
 
   const location = useLocation();
-
 
   const [
     getBandStatsList,
@@ -42,6 +43,7 @@ const SearchForSongs: React.FC<SearchingForSongsProps> = (props) => {
         bandStats.push(JSON.parse(JSON.stringify(bs)));
       });
       setBandStats(bandStats);
+      loadBubbleChartSVG();
     },
   });
 
@@ -52,11 +54,16 @@ const SearchForSongs: React.FC<SearchingForSongsProps> = (props) => {
   const triggerChange = () => {
     //state updates are batched by React which causes a delay. The below gets the true current state.
     setSearchText((state) => {
+      if(state == null) {
+        state = "";
+      }
       setApiSearchText(state);
       getSongs();
       return state;
     });
+  }
 
+  const loadBubbleChartSVG = (() => {
     setBandStats((state) => {
       const id = "bubbleChart" + Math.floor(Date.now() / 1000);
       setBubbleDivId(id);
@@ -72,7 +79,7 @@ const SearchForSongs: React.FC<SearchingForSongsProps> = (props) => {
       }, 500, state);
       return state;
     });
-  }
+  });
 
   useEffect(() => {
     setFocus();
@@ -92,12 +99,12 @@ const SearchForSongs: React.FC<SearchingForSongsProps> = (props) => {
   }, []);
 
   useEffect(() => {
-    const timeout = setTimeout(() => {
+    refTimerTrigger.current = window.setTimeout(() => {
       triggerChange();
-    }, WAIT_INTERVAL)
+    }, 1000);
 
     // if this effect run again, because `value` changed, we remove the previous timeout
-    return () => clearTimeout(timeout)
+    return () => clearTimeout(refTimerTrigger.current);
   }, [searchText]);
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLIonSearchbarElement>) => {
@@ -108,14 +115,19 @@ const SearchForSongs: React.FC<SearchingForSongsProps> = (props) => {
 
   const handleInputChange = (e: CustomEvent<SearchbarChangeEventDetail>) => {
     setInputValue(e.detail.value);
-    var clean = e.detail.value;
-    if(e.detail.value?.toLowerCase().includes("the the") == false
-        &&
-        e.detail.value?.toLowerCase().includes("the ") == true
-        ) {
-          clean = e.detail.value?.replace("The ", "");
-    }
-    setSearchText(clean);
+    clearTimeout(refTimer.current);
+    clearTimeout(refTimerTrigger.current);
+
+    refTimer.current = window.setTimeout(() => {
+      var clean = e.detail.value;
+      if(e.detail.value?.toLowerCase().includes("the the") == false
+          &&
+          e.detail.value?.toLowerCase().includes("the ") == true
+          ) {
+            clean = e.detail.value?.replace("The ", "");
+      }
+      setSearchText(clean);
+    }, 2000);
   };
 
   const handleSearchbarClear = (e: CustomEvent<void>) => {
@@ -138,7 +150,7 @@ const SearchForSongs: React.FC<SearchingForSongsProps> = (props) => {
     { loading, error, data }
   ] = useLazyQuery(GET_SEARCH_RESULTS, {
     fetchPolicy: 'no-cache', nextFetchPolicy: 'no-cache',
-    variables: { searchText: { apiSearchText } }, onCompleted: (data) => {
+    variables: { searchText:  apiSearchText }, onCompleted: (data) => {
       let songs: Song[] = [];
       data.songBySearchText.forEach( (sng: Song)=> {
         songs.push(JSON.parse(JSON.stringify(sng)));
@@ -176,10 +188,9 @@ const SearchForSongs: React.FC<SearchingForSongsProps> = (props) => {
         displayData
         && <SongList showId={true} showScore={true} songs={displayData} editCallback={props.editCallback} showEditButton={props.showEditButton} showDeleteButton={false} />
       }
-      {
-        displayData.length == 0 &&
-        <div className="bubbleChart" id={bubbleDivId}></div>
-      }
+
+      <div className="bubbleChart" id={bubbleDivId} style={{display: displayData.length == 0 ? '' : 'none' }}></div>
+
       <FabToSubmit />
     </>
   );
